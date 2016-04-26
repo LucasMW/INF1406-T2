0f8f7d0d9b6a3c99001ba8e78b1431badb1fbc20
LucasMW <great-lucas@hotmail.com>
2016-04-26 14:22:34 -0300

    removed useless imports

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Scanner;
-
-
 
 public class Main 
 {
@@ -145,7 +140,7 @@ public class Main
 		}
 		return list;
 	}
-	//use this for measurements
+	//use this for time measurements
 	public static void testRoutine(int N,int Mi,int Mf,int q)
 	{
 		
